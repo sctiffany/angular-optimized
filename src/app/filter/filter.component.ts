64ab1321706e13c9c18b690e390cd91b3a6bab5e
Tiffany <tiffany.schinelli@gmail.com>
2025-06-14 17:28:38 +0200

    Mise en place de ChangeDetectionStrategy

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ChangeDetectionStrategy  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl:'./filter.html',
-  styleUrls: ['./filter.css']
+  styleUrls: ['./filter.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FilterComponent {
   @Output() orderChange = new EventEmitter<'asc' | 'desc'>();
